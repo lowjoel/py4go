ec8a996e1be159af49c9353225a43a3db2c5c999
Joel Low <joel.low@grabtaxi.com>
2025-09-30 11:19:52 +0800

    Handle panics across interpreter shutdown
    Calling any Python methods, including determining the string
    representation of the Exception object, will fail after the Python
    interpreter is shut down.
    
    If there is an Exception, materialize the contents before shutting down
    the interpreter.

--- general.go
+++ general.go
@@ -8,16 +8,34 @@ package python
 #include <Python.h>
 */
 import "C"
+import "errors"
 
 func Initialize() {
 	C.Py_Initialize()
 }
 
 func Finalize() error {
-	if C.Py_FinalizeEx() == 0 {
-		return nil
-	} else {
+	var repanic *Exception
+	if panicked := recover(); panicked != nil {
+		if err, ok := panicked.(error); ok {
+			errors.As(err, &repanic)
+		}
+	}
+
+	if repanic != nil {
+		// If this is a Python exception, we need to get the value before we shut down the Python runtime.
+		// Otherwise we will get a segfault.
+		repanic.materialize()
+	}
+
+	finalizeErr := C.Py_FinalizeEx() != 0
+	switch {
+	case repanic != nil:
+		panic(repanic)
+	case finalizeErr:
 		return GetError()
+	default:
+		return nil
 	}
 }
 
